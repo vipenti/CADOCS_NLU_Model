308bfb0a4886e9ba3e7a849c902a145a246ee427
Gianmario Voria <voriagianmario@gmail.com>
2022-06-07 14:39:07 +0200

    implemented date filtering

--- cadocs_service.py
+++ cadocs_service.py
@@ -17,10 +17,17 @@ def predict():
     # message = "hello CADOCS, show me the community smells in the repository https://github.com/tensorflow/ranking from 21/05/2020"
     re_equ = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     ck_url = re.findall(re_equ, message)
+    entities = {
+        "url":"",
+        "date":""
+    }
     if ck_url:
+        entities.update({"url":[i[0] for i in ck_url][0]})
         message = message.replace([i[0] for i in ck_url][0], "LINK")
-
-    result = {"intent": model.give_prediction(message), "entities": ck_url}
+    date = re.findall('\d{2}/\d{2}/\d{4}', message)
+    if date:
+        entities.update({"date":date[0]})
+    result = {"intent": model.give_prediction(message), "entities": entities}
 
     return json.dumps(result)
 
